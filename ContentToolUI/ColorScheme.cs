2de8cccf8293fb5dca1d4610abe94e8317895e7e
Brad Moretti <crth118@gmail.com>
2023-08-23 13:38:57 -0700

    Update color scheme

--- ContentToolUI/ColorScheme.cs
+++ ContentToolUI/ColorScheme.cs
@@ -8,7 +8,18 @@ namespace ContentToolUI
 {
     public static class ColorScheme
     {
-        public static Color Text = Color.Black;
+        public static Color Text;
+        public static Color Background;
+        public static Color TextBox;
+        public static Color Buttons;
+        public static Color ButtonText;
+    }
 
+    public enum ColorSchemes
+    {
+        Tabcorp,
+        MaxSys,
+        Discord,
+        OldSchool
     }
 }
