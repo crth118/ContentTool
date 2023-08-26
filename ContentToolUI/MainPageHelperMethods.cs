71ea3056d14433e6eb8ec79e6e45e145b22c95fe
Brad Moretti <crth118@gmail.com>
2023-08-25 18:09:59 -0700

    Copy duration sets duration for all image types

--- ContentToolUI/MainPageHelperMethods.cs
+++ ContentToolUI/MainPageHelperMethods.cs
@@ -17,9 +17,21 @@ namespace ContentToolUI
         private void SetDurationsForAll(int imageCount, int index, ContentImageType imageType)
         {
             var newDuration = Controls.Find($"{imageType}{ImageDurationControlName}{index}", true).First().Text;
-            for (int i = 0; i <= imageCount; i++)
+            for (int i = 0; i <= TFTimagecount; i++)
             {
-                var duration = Controls.Find($"{imageType}{ImageDurationControlName}{i}", true).First();
+                var duration = Controls.Find($"{ContentImageType.TFT}{ImageDurationControlName}{i}", true).First();
+                duration.Text = newDuration;
+            }
+            
+            for (int i = 0; i <= U2imagecount; i++)
+            {
+                var duration = Controls.Find($"{ContentImageType.U2}{ImageDurationControlName}{i}", true).First();
+                duration.Text = newDuration;
+            }
+            
+            for (int i = 0; i <= U3imagecount; i++)
+            {
+                var duration = Controls.Find($"{ContentImageType.U3}{ImageDurationControlName}{i}", true).First();
                 duration.Text = newDuration;
             }
         }
